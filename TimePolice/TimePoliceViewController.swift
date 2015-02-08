19fee7eb2a719b54dd6c1952451852acdeb4b6ce
perekskog <per.ekskog@gmail.com>
2015-02-08 21:21:20 +0100

    Moved task list to TimePolice

--- TimePolice/TimePoliceViewController.swift
+++ TimePolice/TimePoliceViewController.swift
@@ -1,4 +1,4 @@
-//
+    //
 //  TimePoliceViewController.swift
 //  TimePolice
 //
@@ -21,15 +21,35 @@ class TimePoliceViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+/*
+        taskList = [
+            Task(name: "I F2F"), Task(name: "---"), Task(name: "I Lync"),
+            Task(name: "I Email"), Task(name: "I Ticket"), Task(name: "I Blixt"),
+            Task(name: "P OF"), Task(name: "P Task"), Task(name: "P Ticket"),
+            Task(name: "P US"), Task(name: "P Meeting"), Task(name: "P Other"),
+            Task(name: "N Waste"), Task(name: "---"), Task(name: "N Not work"),
+            Task(name: "N Connect"), Task(name: "N Down"), Task(name: "N Time in"),
+            Task(name: "N Walking"), Task(name: "N Coffee/WC"),  Task(name: "N Other"),
+        ]
 
-    /*
-    // MARK: - Navigation
+*/
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+        let taskList = [
+            Task(name: "I F2F"), Task(name: "---"), Task(name: "I Chat"),
+            Task(name: "I Email"), Task(name: "---"), Task(name: "I Blixt"),
+            Task(name: "P OF"), Task(name: "---"), Task(name: "P Lista"),
+            Task(name: "P Hushåll"), Task(name: "---"), Task(name: "P Other"),
+            Task(name: "N Waste"), Task(name: "---"), Task(name: "N Work"),
+            Task(name: "N Connect"), Task(name: "N Down"), Task(name: "N Time in"),
+            Task(name: "---"), Task(name: "N Coffee/WC"),  Task(name: "N Other"),
+        ]
+        
+
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "TaskPicker" {
+            let vc = segue.destinationViewController as TaskPickerViewController
+            vc.taskList = taskList
+        } 
     }
-    */
 
 }
